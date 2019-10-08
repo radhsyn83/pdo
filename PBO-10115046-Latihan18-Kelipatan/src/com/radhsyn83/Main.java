60efd77f52ab24b6bd3175bffc6b0953acacd53b
radhsyn83 <radhsyn83@gmail.com>
2019-10-08 19:46:33 +0700

    add latihan 18

---
+++ PBO-10115046-Latihan18-Kelipatan/src/com/radhsyn83/Main.java
@@ -0,0 +1,25 @@
+package com.radhsyn83;
+/*
+ *
+ * Fathur Radhy
+ * 10115046
+ * PDO11K
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        double hasil;
+
+        hasil = 0;
+
+        while (hasil < 35.0) {
+            hasil = hasil+3.5;
+
+            System.out.println(""+ hasil);
+        }
+
+    }
+}
