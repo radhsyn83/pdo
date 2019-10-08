482072ed4fa5cbe4d9888f54b9ffafc802f32402
radhsyn83 <radhsyn83@gmail.com>
2019-10-08 21:03:10 +0700

    add latihan 26

---
+++ PBO-10115046-Latihan27-HurufBesarKecil/src/com/radhsyn83/Main.java
@@ -0,0 +1,15 @@
+package com.radhsyn83;
+
+/*
+ *
+ * Fathur Radhy
+ * 10115046
+ * PDO11K
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
